9cdc0f47b3ee880a5b0694b3b49579059df6f715
lloam <2196143404@qq.com>
2021-09-11 17:37:04 +0800

    :art:主题模式

---
+++ rabbitmq-hello/src/main/java/com/mao/rabbitmq/seven/ReceiveLogsTopic01.java
@@ -0,0 +1,41 @@
+package com.mao.rabbitmq.seven;
+
+import com.mao.rabbitmq.utils.RabbitMQUtils;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DeliverCallback;
+
+/**
+ * Author: lloam
+ * Date: 2021/9/11 17:10
+ * Description:
+ * 声明主题模式交换机 与绑定队列
+ * 消费者
+ */
+public class ReceiveLogsTopic01 {
+
+    public static final String EXCHANGE_NAME = "topic_logs";
+
+    public static void main(String[] args) throws Exception {
+
+        Channel channel = RabbitMQUtils.getChannel();
+
+        // 声明交换机
+        channel.exchangeDeclare(EXCHANGE_NAME, "topic");
+
+        // 声明队列
+        String queueName = "Q1";
+        channel.queueDeclare(queueName, false, false, false, null);
+        // 绑定队列
+        channel.queueBind(queueName, EXCHANGE_NAME, "*.orange.*");
+        System.out.println("Q1 等待接收消息......");
+
+        DeliverCallback deliverCallback = (consumerTag, message) -> {
+            System.out.println(new String(message.getBody(), "UTF-8"));
+            System.out.println("接收队列：" + queueName + "  绑定键：" + message.getEnvelope().getRoutingKey());
+        };
+
+        // 接收消息
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {});
+
+    }
+}
